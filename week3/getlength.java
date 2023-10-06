549652711718740ee911a6282c974b1c8c83c909
27sh <120109008+27sh@users.noreply.github.com>
2023-10-06 15:21:15 +0900

    Create getlength.java

---
+++ week3/getlength.java
@@ -0,0 +1,28 @@
+package com.example.week2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText edittext_input=findViewById(R.id.edittext_input);
+        Button button_convert=findViewById(R.id.button_convert);
+        TextView textview_display=findViewById(R.id.textview_display);
+        button_convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = edittext_input.getText().toString();
+                String l = Integer.toString(s.length());
+                textview_display.setText(l);
+            }
+        });
+    }
+}
