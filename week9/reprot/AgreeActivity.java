d69df5a8673310812059353688994832b9df84b3
27sh <120109008+27sh@users.noreply.github.com>
2023-12-04 19:44:46 +0900

    Create AgreeActivity.java

---
+++ week9/reprot/AgreeActivity.java
@@ -0,0 +1,12 @@
+package com.example.week10;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AgreeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
