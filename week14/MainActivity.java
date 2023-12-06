0d620174e1af5449a11ead6c2e55f963c41dedf4
27sh <120109008+27sh@users.noreply.github.com>
2023-12-06 21:26:05 +0900

    Create MainActivity.java

---
+++ week14/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.week10;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Test");
+        Button btn_start = findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(intent); // 인텐트에 해당하는 액티비티 실행
+            }
+        });
+    }
+}
